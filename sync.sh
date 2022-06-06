97aea920791ecbeefaf4385d48af2722708d9727
Kuan Cheang <kuancheang@asqimacau.com>
2022-06-06 13:15:09 +0800

    Moving the required files into cis folder

--- sync.sh
+++ sync.sh
@@ -5,5 +5,16 @@ if test -z $1; then
     exit 1
 fi
 
-scp -r * root@$1:/root/
-ssh root@$1 bash -c "./run.sh"
+ENV=production
+if [ -n "$2" -a \( "$2" == "staging" -o "$2" == "production" \) ]; then
+    ENV=$2
+elif [ -n "$2" -a \( "$2" != "staging" -a "$2" != "production" \) ]; then
+    printf '%s\n' "Invalid environment option for the second argument." >&2
+    exit 1
+fi
+
+echo "Copying the required file into the /root path"
+scp -qr cis/ root@$1:/root/cis
+echo "Start run the script with the environment \"$ENV\"..."
+ssh root@$1 bash -c "/root/cis/run.sh $ENV"
+echo ""
